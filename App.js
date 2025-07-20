4d39e4c309dc8b33df751866a885afb456a64ff2
Muhammad-Allyan <muhammadallyanahmad25@gmail.com>
2025-07-20 21:33:14 +0500

    updated commit

--- App.js
+++ App.js
@@ -28,82 +28,82 @@ const HeaderCompoent = () => {
 
 // Restaurant Card Component Starts
 
-const resObj = {
-  type: "restaurant",
-  info: {
-    id: "1013620",
-    name: "Big Bowl",
-    cloudinaryImageId:
-      "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/24/cf06adc4-a816-4170-b023-83a41f36db89_1013620.jpg",
-    locality: "Koramangala",
-    areaName: "80 Feet Road",
-    costForTwo: "₹250 for two",
-    cuisines: ["Chinese", "Tibetan", "Desserts"],
-    avgRating: 4.5,
-    parentId: "434792",
-    avgRatingString: "4.5",
-    totalRatingsString: "253",
-    slaString: "20-25 mins",
-    promoted: true,
-    adTrackingId:
-      "cid=31239797~p=0~adgrpid=31239797#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=1013620~plpr=COLLECTION~eid=78b2acc8-d5b1-4430-a65f-609894219115~srvts=1753003930567~collid=83639",
-    sla: {
-      deliveryTime: 23,
-      lastMileTravel: 0.7,
-      serviceability: "SERVICEABLE",
-      slaString: "20-25 mins",
-      lastMileTravelString: "0.7 km",
-      iconType: "ICON_TYPE_EMPTY",
-    },
-    availability: {
-      nextCloseTime: "2025-07-20 23:00:00",
-      opened: true,
-    },
-    badges: {},
-    isOpen: true,
-    type: "F",
-    badgesV2: {
-      entityBadges: {
-        imageBased: {},
-        textExtendedBadges: {},
-        textBased: {},
-      },
-    },
-    aggregatedDiscountInfoV3: {
-      header: "ITEMS",
-      subHeader: "AT ₹129",
-      logoCtx: {
-        text: "BENEFITS",
-      },
-    },
-    orderabilityCommunication: {
-      title: {},
-      subTitle: {},
-      message: {},
-      customIcon: {},
-      commsStyling: {},
-    },
-    differentiatedUi: {
-      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-      differentiatedUiMediaDetails: {
-        mediaType: "ADS_MEDIA_ENUM_IMAGE",
-        lottie: {},
-        video: {},
-      },
-    },
-    reviewsSummary: {},
-    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-    isNewlyOnboarded: true,
-    restaurantOfferPresentationInfo: {},
-    externalRatings: {
-      aggregatedRating: {
-        rating: "--",
-      },
-    },
-    ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
-    campaignId: "31239797",
-  },
-};
+// const resObj = {
+//   type: "restaurant",
+//   info: {
+//     id: "1013620",
+//     name: "Big Bowl",
+//     cloudinaryImageId:
+//       "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/24/cf06adc4-a816-4170-b023-83a41f36db89_1013620.jpg",
+//     locality: "Koramangala",
+//     areaName: "80 Feet Road",
+//     costForTwo: "₹250 for two",
+//     cuisines: ["Chinese", "Tibetan", "Desserts"],
+//     avgRating: 4.5,
+//     parentId: "434792",
+//     avgRatingString: "4.5",
+//     totalRatingsString: "253",
+//     slaString: "20-25 mins",
+//     promoted: true,
+//     adTrackingId:
+//       "cid=31239797~p=0~adgrpid=31239797#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=1013620~plpr=COLLECTION~eid=78b2acc8-d5b1-4430-a65f-609894219115~srvts=1753003930567~collid=83639",
+//     sla: {
+//       deliveryTime: 23,
+//       lastMileTravel: 0.7,
+//       serviceability: "SERVICEABLE",
+//       slaString: "20-25 mins",
+//       lastMileTravelString: "0.7 km",
+//       iconType: "ICON_TYPE_EMPTY",
+//     },
+//     availability: {
+//       nextCloseTime: "2025-07-20 23:00:00",
+//       opened: true,
+//     },
+//     badges: {},
+//     isOpen: true,
+//     type: "F",
+//     badgesV2: {
+//       entityBadges: {
+//         imageBased: {},
+//         textExtendedBadges: {},
+//         textBased: {},
+//       },
+//     },
+//     aggregatedDiscountInfoV3: {
+//       header: "ITEMS",
+//       subHeader: "AT ₹129",
+//       logoCtx: {
+//         text: "BENEFITS",
+//       },
+//     },
+//     orderabilityCommunication: {
+//       title: {},
+//       subTitle: {},
+//       message: {},
+//       customIcon: {},
+//       commsStyling: {},
+//     },
+//     differentiatedUi: {
+//       displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//       differentiatedUiMediaDetails: {
+//         mediaType: "ADS_MEDIA_ENUM_IMAGE",
+//         lottie: {},
+//         video: {},
+//       },
+//     },
+//     reviewsSummary: {},
+//     displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//     isNewlyOnboarded: true,
+//     restaurantOfferPresentationInfo: {},
+//     externalRatings: {
+//       aggregatedRating: {
+//         rating: "--",
+//       },
+//     },
+//     ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+//     campaignId: "31239797",
+//   },
+// };
 
 const resList = [
   {
@@ -207,6 +207,252 @@ const resList = [
     },
     widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
   },
+  {
+    // @type: "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "78511",
+      name: "NIC Ice Creams",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/7/8/13558e62-ff1d-414e-a67d-0634893a53c4_78511.JPG",
+      locality: "8th Block",
+      areaName: "Koramangala",
+      costForTwo: "₹120 for two",
+      cuisines: ["Ice Cream", "Desserts"],
+      avgRating: 4.8,
+      veg: true,
+      parentId: "6249",
+      avgRatingString: "4.8",
+      totalRatingsString: "9.6K+",
+      sla: {
+        deliveryTime: 23,
+        lastMileTravel: 1.5,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "1.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-07-20 23:00:00",
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: "Ratnesh_Badges/test2.png",
+            shortDescription: "Perfect ice cream delivery",
+            fontColor: "#7E808C",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "",
+                  fontColor: "#7E808C",
+                  iconId: "Ratnesh_Badges/test2.png",
+                  shortDescription: "Perfect ice cream delivery",
+                },
+              },
+            ],
+          },
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹124",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-6c740632-03e3-4032-925e-c874ee6dd35c",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/bangalore/nic-ice-creams-8th-block-koramangala-rest78511",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    // @type: "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "671928",
+      name: "KFC",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/377e9327-3ca7-48ac-b36c-96f5bf6e186a_671928.JPG",
+      locality: "7th Block",
+      areaName: "Koramangla",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Fast Food", "Rolls & Wraps"],
+      avgRating: 4.2,
+      parentId: "547",
+      avgRatingString: "4.2",
+      totalRatingsString: "5.2K+",
+      sla: {
+        deliveryTime: 31,
+        lastMileTravel: 1.2,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "1.2 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-07-21 03:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹59",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-6c740632-03e3-4032-925e-c874ee6dd35c",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/bangalore/kfc-7th-block-koramangla-rest671928",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    // @type: "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "1013619",
+      name: "Chinese Wok",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/24/15345730-3e92-4301-8bf8-d3d327d96ce2_1013619.jpg",
+      locality: "Koramangala",
+      areaName: "80 Feet Road",
+      costForTwo: "₹250 for two",
+      cuisines: ["Chinese", "Asian", "Tibetan", "Desserts"],
+      avgRating: 4.3,
+      parentId: "61955",
+      avgRatingString: "4.3",
+      totalRatingsString: "409",
+      sla: {
+        deliveryTime: 39,
+        lastMileTravel: 0.7,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "0.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-07-20 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹129",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      isNewlyOnboarded: true,
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-6c740632-03e3-4032-925e-c874ee6dd35c",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/bangalore/chinese-wok-koramangala-80-feet-road-rest1013619",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
 ];
 
 const RestroCardComponent = (props) => {
@@ -238,7 +484,7 @@ const BodyComponent = () => {
     <div className="body">
       <div className="search-bar">Search</div>
       <div className="restro-container">
-        <RestroCardComponent resData={resObj} />
+        <RestroCardComponent resData={resList[0]} />
       </div>
     </div>
   );

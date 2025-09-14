46ec649f3eb7531c2ea2cd45096abe820c96e568
Muhammad-Allyan <muhammadallyanahmad25@gmail.com>
2025-09-14 23:03:20 +0500

    sunday commit

--- src/Components/BodyComponent.jsx
+++ src/Components/BodyComponent.jsx
@@ -14,8 +14,18 @@ const BodyComponent = () => {
   let [listOfRestaurants, setListOfRestaurants] = useState(resList);
 
   useEffect(() => {
-    console.log(`useEffect() called!`);
+    // console.log(`useEffect() called!`);
+    fetchData();
   }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfRestaurants(json?.data?.cards?.[4].card?.card?.gridElements?.infoWithStyle?.restaurants);
+    console.log(json);
+  };
 
   console.log("Body Rendered!");
 
@@ -26,7 +36,7 @@ const BodyComponent = () => {
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurants.filter(
-              (res) => res.info.avgRating > 4
+              (res) => res.info.avgRating > 4.2
             );
             setListOfRestaurants(filteredList);
             console.log(listOfRestaurants);
